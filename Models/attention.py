80aaf5a7ade8e0567346694e7edbe44f4ebd7376
asdf3118 <asdf2kr@naver.com>
2019-10-18 12:44:52 +0900

    Add batchNorm2d in BAM

--- Models/attention.py
+++ Models/attention.py
@@ -14,16 +14,18 @@ class BAM(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
         self.fc1 = nn.Linear(in_features=in_channel, out_features=self.hid_channel)
-        # self.bn1_1d = nn.BatchNorm1d(self.hid_channel)
+        self.bn1_1d = nn.BatchNorm1d(self.hid_channel)
         self.fc2 = nn.Linear(in_features=self.hid_channel, out_features=in_channel)
-        # self.bn2_1d = nn.BatchNorm1d(self.hid_channel)
-        self.bn_1d = nn.BatchNorm1d(in_channel)
+        self.bn2_1d = nn.BatchNorm1d(in_channel)
 
         self.conv1 = conv1x1(in_channel, self.hid_channel)
+        self.bn1_2d = nn.BatchNorm2d(self.hid_channel)
         self.conv2 = conv3x3(self.hid_channel, self.hid_channel, stride=1, padding=self.dilation, dilation=self.dilation)
+        self.bn2_2d = nn.BatchNorm2d(self.hid_channel)
         self.conv3 = conv3x3(self.hid_channel, self.hid_channel, stride=1, padding=self.dilation, dilation=self.dilation)
+        self.bn3_2d = nn.BatchNorm2d(self.hid_channel)
         self.conv4 = conv1x1(self.hid_channel, 1)
-        self.bn_2d = nn.BatchNorm2d(1)
+        self.bn4_2d = nn.BatchNorm2d(1)
 
     def forward(self, x):
         # Channel attention
@@ -31,31 +33,34 @@ class BAM(nn.Module):
         Mc = Mc.view(Mc.size(0), -1)
 
         Mc = self.fc1(Mc)
+        Mc = self.bn1_1d(Mc)
         Mc = self.relu(Mc)
 
         Mc = self.fc2(Mc)
+        Mc = self.bn2_1d(Mc)
         Mc = self.relu(Mc)
 
-        Mc = self.bn_1d(Mc)
         Mc = Mc.view(Mc.size(0), Mc.size(1), 1, 1)
 
         # Spatial attention
         Ms = self.conv1(x)
+        Ms = self.bn1_2d(Ms)
         Ms = self.relu(Ms)
 
         Ms = self.conv2(Ms)
+        Ms = self.bn2_2d(Ms)
         Ms = self.relu(Ms)
 
         Ms = self.conv3(Ms)
+        Ms = self.bn3_2d(Ms)
         Ms = self.relu(Ms)
 
         Ms = self.conv4(Ms)
+        Ms = self.bn4_2d(Ms)
         Ms = self.relu(Ms)
 
-        Ms = self.bn_2d(Ms)
         Ms = Ms.view(x.size(0), 1, x.size(2), x.size(3))
-
-        Mf = self.sigmoid(Mc * Ms)
+        Mf = 1 + self.sigmoid(Mc * Ms)
         return x + (x * Mf)
 
 #To-do:
